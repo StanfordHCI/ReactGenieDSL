006553692996cdcae641fd1d44e33962045a43b4
Karina Li <104478165+karinakli@users.noreply.github.com>
2023-05-25 14:46:33 -0700

    Fix Order test

--- lib/decorators/__test__/decorators.test.ts
+++ lib/decorators/__test__/decorators.test.ts
@@ -69,12 +69,11 @@ test("Food Descriptor", async () => {
 
 test("Order Descriptor", async () => {
     initGenie();
-    const { Order, Restaurant, DateTime } = await import("../../__test__/example_descriptor");
+    const { Order, DateTime } = await import("../../__test__/example_descriptor");
     compareClassDescriptor(Order.ClassDescriptor, Order._ClassDescriptor);
     Order.all();
-    const mcdonalds = Restaurant.GetObject({name: "McDonald's"});
-    const order1 = mcdonalds.orders[0];
-    const order2 = mcdonalds.orders[1];
+    const order1 = Order.GetObject({dateTime: new DateTime({ year: 2020, month: 1, day: 1, hour: 13, minute: 0 })});
+    const order2 = Order.GetObject({datetime: new DateTime({ year: 2020, month: 2, day: 1, hour: 14, minute: 0 })});
     order2.dateTime = new DateTime({ year: 2023, month: 1, day: 1, hour: 13, minute: 0 }),
         assert.equal(order1.dateTime, new DateTime({ year: 2023, month: 1, day: 1, hour: 13, minute: 0 }));
     order2.dateTime = new DateTime({ year: 2023, month: 1, day: 2, hour: 13, minute: 0 }),
