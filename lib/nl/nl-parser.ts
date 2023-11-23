26d8415c572a77622251becdce9dd489b19e2489
Jackie Yang <jackie@jackieyang.me>
2023-11-23 01:22:03 -0800

    Updated package

--- lib/nl/nl-parser.ts
+++ lib/nl/nl-parser.ts
@@ -84,7 +84,7 @@ export class NlParser {
   ): Promise<string | null> {
     const prompt = this.prompt.response_prompt(nl, parsed, result);
     const response = await this.openAiApi.createCompletion({
-      model: "text-davinci-003",
+      model: "gpt-3.5-turbo-instruct",
       prompt: prompt,
       temperature: 0.7,
       max_tokens: 512,
